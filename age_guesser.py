058dae7d1a360ae71eaf2ea1c2fd9926c8643bef
Emma Tkachuk <d00524364@utahtech.edu>
2025-08-25 19:42:33 -0600

    added younger or older feature and better line spacing

--- age_guesser.py
+++ age_guesser.py
@@ -1,22 +1,30 @@
 import random
 
 print()
-
 print("~~ Hello, welcome to Age Guesser! Let's guess your age. ~~")
-
 print()
+
 name = input("-- What is your name? _")
 
 guessed = False
 
+lower = 15
+upper = 30
+
 while not guessed:
-    age = random.randint(15,30)
+    age = random.randint(lower,upper)
     
-    answer = input("Are you " + str(age) + "? (y/n): ")
+    answer = input(f"Are you {age}? (y/n): _")
     
     if answer == 'y':
-        print("YAY! " + name + " is " + str(age) + " years old.")
+        print(f"YAY! {name} is {age} years old.")
         guessed = True
 
     else:
         print("Rats.")
+        feedback = input(f"-- Are you older or younger than {age}? (older/younger): _")
+
+        if feedback == 'older':
+            lower = age + 1
+        elif feedback == 'younger':
+            upper = age - 1
